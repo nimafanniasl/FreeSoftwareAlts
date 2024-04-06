3442b76a103f08692b5b4f06c37dba56456b2881
Nima <smartnimaco@gmail.com>
2024-04-06 19:13:38 +0330

    Added the wiki

--- Scripts/auto_screenshot_from_fdroid.py
+++ Scripts/auto_screenshot_from_fdroid.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-TAG_START = "\n<!-- F-droid app screenshots -start -->\n"
+TAG_START = "<!-- F-droid app screenshots -start -->\n"
 TAG_END = "\n<!-- F-droid app screenshots -end -->"
 
 TAG_NO_IMAGE = "\n<!-- F-droid app screenshots -NoImageExists -->\n"
@@ -14,13 +14,16 @@ IMG_HEIGHT = "auto"
 
 # --------------------------------------------------- #
 
+
 # Define a function to get the package name from the F-Droid URL
 def get_package_name(url: str):
-    return url.split('/')[-2].replace("/", "")
+    return url.split("/")[-2].replace("/", "")
+
 
 def generate_img_tag(img_url):
     img_tag = f'<img src="{img_url}" width="{IMG_WIDTH}" height="{IMG_HEIGHT}">'
     return img_tag
+
 
 def get_pics(package_name):
     image_links = []
@@ -48,14 +51,15 @@ def get_pics(package_name):
 
     return image_tags_str
 
+
 # Define the main function to process the file
 def process_file(file_path):
     # Open the file for reading
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
     # Regular expression pattern to match F-Droid links
-    fdroid_pattern = re.compile(r'\((https?://(?:www\.)?f-droid\.org[^)]+)\)')
+    fdroid_pattern = re.compile(r"\((https?://(?:www\.)?f-droid\.org[^)]+)\)")
 
     # Iterate through the lines
     for i, line in enumerate(lines):
@@ -63,15 +67,20 @@ def process_file(file_path):
         if match:
             package_name = get_package_name(match.group(1))
             # Check if additional text exists after the line
-            if i < len(lines) - 1 and not lines[i+2].startswith(TAG_START) and not lines[i+2].startswith(TAG_NO_IMAGE):
+            if (
+                i < len(lines) - 1
+                and not lines[i + 1].startswith(TAG_START)
+                and not lines[i + 1].startswith(TAG_NO_IMAGE)
+            ):
                 # Call get_pics function to retrieve additional text
                 additional_text = get_pics(package_name)
                 # Insert additional text after the line
-                lines.insert(i+1, additional_text + '\n')
+                lines.insert(i + 1, additional_text + "\n")
 
     # Write the modified content back to the file
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.writelines(lines)
 
+
 if __name__ == "__main__":
-    process_file("Android.md")
+    process_file("docs/اندروید.md")
